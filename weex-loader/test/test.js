61abe5ccb34473f6a86634801c16594185211ec7
Jinjiang <zhaojinjiang@me.com>
2016-06-21 09:09:54 +0800

    updated test cases

--- weex-loader/test/test.js
+++ weex-loader/test/test.js
@@ -40,6 +40,25 @@ describe('loader', () => {
     expect(actualJson).eql(expectJson);
   });
 
+  it('element tag, 3rd-party js, implicit component', function() {
+    var name = 'b.js';
+
+    var actualCodePath = path.resolve(__dirname, 'actual', name);
+    var actualCodeContent = fs.readFileSync(actualCodePath);
+
+    var expectCodePath = path.resolve(__dirname, 'expect', name);
+    var expectCodeContent = fs.readFileSync(expectCodePath);
+
+
+    var actualResult = createInstance('actual/' + name, actualCodeContent);
+    var actualJson = getRoot('actual/' + name);
+
+    var expectResult = createInstance('expect/' + name, expectCodeContent);
+    var expectJson = getRoot('expect/' + name);
+
+    expect(actualJson).eql(expectJson);
+  });
+
   it('with config & data case', function() {
     var name = 'z.js';
 
